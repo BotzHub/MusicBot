39490b93da1cf52369cddcdd511d13cf8dfbeb50
Botz Hub <botzhub01@gmail.com>
2025-05-21 18:29:24 +0530

    Update bot.py

--- YMusic/core/bot.py
+++ YMusic/core/bot.py
@@ -9,7 +9,7 @@ api_hash: str = config.API_HASH
 session_string: str = config.SESSION_STRING
 
 YMusicBot = Client(
-    name="YMusic", api_id=api_id, api_hash=api_hash, session_string=session_string
+    name="YMusic", api_id=api_id, api_hash=api_hash, session_string=session_string, device_model="Music-UserBot"
 )
 
 YMusicUser = PyTgCalls(YMusicBot)
